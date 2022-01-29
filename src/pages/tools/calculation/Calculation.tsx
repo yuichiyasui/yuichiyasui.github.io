1366ead8d468cc399caca9be530f460ad132f4e7
yuichiyasui <yassan.b.a.w@gmail.com>
2022-01-29 23:42:04 +0900

    /tools/calculationページを追加

---
+++ src/pages/tools/calculation/Calculation.tsx
@@ -0,0 +1,3 @@
+export const Calculation = () => {
+  return <div>hoge</div>;
+};
--- src/router.tsx
+++ src/router.tsx
@@ -4,11 +4,17 @@ import { Route, Routes } from 'react-router-dom';
 const Top = lazy(() =>
   import('~/pages/top').then((module) => ({ default: module.Top })),
 );
+const Calculation = lazy(() =>
+  import('~/pages/tools/calculation').then((module) => ({
+    default: module.Calculation,
+  })),
+);
 
 export const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Top />} />
+      <Route path="/tools/calculation" element={<Calculation />} />
     </Routes>
   );
 };
