a82eaeb0e1c636a4ccedfee348191309afaba129
yuichiyasui <yassan.b.a.w@gmail.com>
2022-01-30 04:09:05 +0900

    状態管理のやり方を見直し

--- src/pages/tools/calculation/Calculation.tsx
+++ src/pages/tools/calculation/Calculation.tsx
@@ -1,51 +1,108 @@
-import { ChangeEvent, FormEvent, useEffect, useState } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
+import { enableMapSet } from "immer";
+import { useImmerReducer } from "use-immer";
 import { DefaultLayout } from "~/components/layouts/DefaultLayout";
+
+enableMapSet();
 
 const sectionMap = {
   member: "member",
   price: "price",
 };
 
+type MemberName = string;
+type Price = number;
+
+type AddMember = {
+  type: "ADD_MEMBER";
+  payload: { name: MemberName };
+};
+type RemoveMember = {
+  type: "REMOVE_MEMBER";
+  payload: { name: MemberName };
+};
+type UpdateMemberPrice = {
+  type: "UPDATE_MEMBER_PRICE";
+  payload: { name: MemberName; price: Price };
+};
+type UpdateError = {
+  type: "UPDATE_ERROR";
+  payload: { errorMessage: string };
+};
+
+type Action = AddMember | RemoveMember | UpdateMemberPrice | UpdateError;
+type State = {
+  members: Map<MemberName, Price>;
+  error: string;
+};
+const reducer = (draft: State, { payload, type }: Action) => {
+  switch (type) {
+    case "ADD_MEMBER":
+      draft.members.set(payload.name, 0);
+      break;
+    case "REMOVE_MEMBER":
+      draft.members.delete(payload.name);
+      break;
+    case "UPDATE_ERROR":
+      draft.error = payload.errorMessage;
+      break;
+    case "UPDATE_MEMBER_PRICE":
+      draft.members.set(payload.name, payload.price);
+      break;
+  }
+};
+
+const initialState: State = {
+  members: new Map<MemberName, Price>(),
+  error: "",
+};
+
 export const Calculation = () => {
-  const [members, setMembers] = useState<string[]>([]);
+  const [state, dispatch] = useImmerReducer(reducer, initialState);
+
   const [member, setMember] = useState<string>("");
-  const memberError = members.includes(member)
-    ? "すでに追加済みのメンバーです"
-    : "";
   const handleMember = (event: ChangeEvent<HTMLInputElement>) => {
     const { value } = event.target;
+    let errorMessage;
+    if (!value) {
+      errorMessage = "メンバー名を入力してください";
+    } else if (state.members.has(value)) {
+      errorMessage = "すでに追加済みのメンバーです";
+    } else {
+      errorMessage = "";
+    }
+    dispatch({
+      type: "UPDATE_ERROR",
+      payload: { errorMessage },
+    });
     setMember(value);
   };
   const submitMember = (event: FormEvent) => {
     event.preventDefault();
-    if (memberError) return;
-    setMembers([...members, member]);
+    if (state.error) return;
+    dispatch({ type: "ADD_MEMBER", payload: { name: member } });
     setMember("");
   };
-  const removeMember = (member: string) => {
-    const removedMembers = members.filter((m) => m !== member);
-    setMembers(removedMembers);
-  };
 
-  const [prices, setPrices] = useState<number[]>([]);
-  const handlePrice = (event: ChangeEvent<HTMLInputElement>, index: number) => {
+  const handlePrice = (
+    event: ChangeEvent<HTMLInputElement>,
+    name: MemberName,
+  ) => {
     const { value } = event.target;
-    if (Number.isNaN(Number(value))) return;
-    const currentPrices = [...prices];
-    currentPrices[index] = Number(value);
-    setPrices(currentPrices);
+    const price = Number(value);
+    if (Number.isNaN(price) || price > 999999) return;
+    dispatch({ type: "UPDATE_MEMBER_PRICE", payload: { name, price } });
   };
 
-  useEffect(() => {
-    if (members.length !== prices.length) {
-      setPrices(Array(members.length).fill(0));
-    }
-  }, [members.length, prices.length]);
-
-  const totalPrice = prices.reduce((acc, price) => {
-    return acc + price;
+  const members = Array.from(state.members).map((member) => ({
+    name: member[0],
+    price: member[1],
+  }));
+  const totalPrice = members.reduce((acc, member) => {
+    return acc + member.price;
   }, 0);
-  const averagePrice = Math.round(totalPrice / members.length);
+  const averagePrice =
+    totalPrice && members.length ? Math.round(totalPrice / members.length) : 0;
 
   return (
     <DefaultLayout>
@@ -63,7 +120,6 @@ export const Calculation = () => {
           <li>それぞれの使用金額を入力</li>
           <li>計算結果が表示される</li>
         </ol>
-        <p>計算結果はLINEでシェアできます。</p>
       </section>
 
       <section id={sectionMap.member} className="mb-10">
@@ -79,8 +135,8 @@ export const Calculation = () => {
               className="focus:shadow-outline mb-2 w-full appearance-none rounded border py-2 px-3 leading-tight text-gray-700 shadow focus:outline-none"
             />
             <small className="block">（例）やっしー</small>
-            {memberError && (
-              <small className="text-red-500">{memberError}</small>
+            {state.error && (
+              <small className="text-red-500">{state.error}</small>
             )}
           </div>
           <button
@@ -91,19 +147,28 @@ export const Calculation = () => {
           </button>
         </form>
         <h3 className="mb-3 text-lg font-bold">メンバーリスト</h3>
-        {members.length ? (
+        {state.members.size ? (
           <ol className="list-inside list-decimal">
             {members.map((member) => {
               return (
-                <li key={member}>
+                <li key={member.name}>
                   <div className="inline-flex items-center">
-                    <p className="mr-3 text-sm">{member}</p>
+                    <p className="mr-3 text-sm">{member.name}</p>
                     <button
                       type="button"
-                      className="h-5 w-5 rounded-full bg-red-500 text-sm font-bold leading-[0] text-white hover:bg-red-700"
-                      onClick={() => removeMember(member)}
+                      className="inline-grid h-5 w-5 place-items-center rounded-full bg-red-500 hover:bg-red-700"
+                      title={`${member.name}をメンバーから削除する`}
+                      aria-label="削除"
+                      onClick={() =>
+                        dispatch({
+                          type: "REMOVE_MEMBER",
+                          payload: { name: member.name },
+                        })
+                      }
                     >
-                      ×
+                      <span className="text-[10px] font-bold text-white">
+                        ×
+                      </span>
                     </button>
                   </div>
                 </li>
@@ -118,18 +183,23 @@ export const Calculation = () => {
       <section id={sectionMap.price} className="mb-10">
         <h2 className="mb-3 text-xl font-bold">使用金額を入力</h2>
         <p className="mb-3 text-sm">半角数字のみ入力可能です。</p>
-
-        {members.map((member, index) => {
+        {members.map((member) => {
           return (
-            <div key={member}>
-              <p>{member}</p>
-              <input
-                type="text"
-                name="price"
-                inputMode="decimal"
-                onChange={(event) => handlePrice(event, index)}
-                className="focus:shadow-outline mb-2 w-full appearance-none rounded border py-2 px-3 leading-tight text-gray-700 shadow focus:outline-none"
-              />
+            <div key={member.name} className="mb-2">
+              <p>{member.name}</p>
+              <div className="relative">
+                <span className="absolute top-1/2 left-2 -translate-y-1/2 text-gray-400">
+                  ¥
+                </span>
+                <input
+                  type="text"
+                  name="price"
+                  inputMode="decimal"
+                  value={state.members.get(member.name)}
+                  onChange={(event) => handlePrice(event, member.name)}
+                  className="focus:shadow-outline w-[100px] appearance-none rounded border py-2 pr-3 pl-5 text-right leading-tight text-gray-700 shadow focus:outline-none"
+                />
+              </div>
             </div>
           );
         })}
@@ -150,21 +220,6 @@ export const Calculation = () => {
             currency: "JPY",
           })}
         </p>
-        <ol>
-          {members.map((member, index) => {
-            return (
-              <li key={member}>
-                <p>{member}</p>
-                <p>
-                  {(prices[index] - averagePrice).toLocaleString("ja-JP", {
-                    style: "currency",
-                    currency: "JPY",
-                  })}
-                </p>
-              </li>
-            );
-          })}
-        </ol>
       </section>
     </DefaultLayout>
   );
