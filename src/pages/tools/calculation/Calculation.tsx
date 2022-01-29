e0c0df9cd195c9f08f8e5a26b907ce27ab79e5b1
yuichiyasui <yassan.b.a.w@gmail.com>
2022-01-30 01:59:43 +0900

    入力処理周りを作成

--- src/pages/tools/calculation/Calculation.tsx
+++ src/pages/tools/calculation/Calculation.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, FormEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useEffect, useState } from "react";
 import { DefaultLayout } from "~/components/layouts/DefaultLayout";
 
 const sectionMap = {
@@ -27,18 +27,38 @@ export const Calculation = () => {
     setMembers(removedMembers);
   };
 
+  const [prices, setPrices] = useState<number[]>([]);
+  const handlePrice = (event: ChangeEvent<HTMLInputElement>, index: number) => {
+    const { value } = event.target;
+    if (Number.isNaN(Number(value))) return;
+    const currentPrices = [...prices];
+    currentPrices[index] = Number(value);
+    setPrices(currentPrices);
+  };
+
+  useEffect(() => {
+    if (members.length !== prices.length) {
+      setPrices(Array(members.length).fill(0));
+    }
+  }, [members.length, prices.length]);
+
+  const totalPrice = prices.reduce((acc, price) => {
+    return acc + price;
+  }, 0);
+  const averagePrice = Math.round(totalPrice / members.length);
+
   return (
     <DefaultLayout>
       <h1 className="mb-8 text-center text-4xl font-bold">割り勘計算ツール</h1>
-      <p>
+      <p className="mb-10">
         飲み会やBBQ、キャンプなどイベントの時にみんなでお金をそれぞれ出し合った際に計算するの面倒ですよね。
         <br />
         そんな面倒な計算をスッと終わらせてくれるツールがこちらです。
       </p>
 
-      <section>
-        <h2>使い方</h2>
-        <ol>
+      <section className="mb-10">
+        <h2 className="mb-3 text-xl font-bold">使い方</h2>
+        <ol className="mb-3 list-inside list-decimal">
           <li>割り勘するメンバーを追加</li>
           <li>それぞれの使用金額を入力</li>
           <li>計算結果が表示される</li>
@@ -46,37 +66,105 @@ export const Calculation = () => {
         <p>計算結果はLINEでシェアできます。</p>
       </section>
 
-      <section id={sectionMap.member}>
-        <h2>メンバー名を入力</h2>
-        <form onSubmit={submitMember}>
-          <input
-            type="text"
-            name="memberName"
-            placeholder="メンバー名"
-            value={member}
-            onChange={handleMember}
-          />
-          <small className="block">（例）やっしー</small>
-          {memberError && <small className="text-red-500">{memberError}</small>}
-          <button type="submit">追加</button>
+      <section id={sectionMap.member} className="mb-10">
+        <h2 className="mb-3 text-xl font-bold">メンバー名を入力</h2>
+        <form onSubmit={submitMember} className="mb-4">
+          <div className="mb-2">
+            <input
+              type="text"
+              name="memberName"
+              placeholder="メンバー名"
+              value={member}
+              onChange={handleMember}
+              className="focus:shadow-outline mb-2 w-full appearance-none rounded border py-2 px-3 leading-tight text-gray-700 shadow focus:outline-none"
+            />
+            <small className="block">（例）やっしー</small>
+            {memberError && (
+              <small className="text-red-500">{memberError}</small>
+            )}
+          </div>
+          <button
+            type="submit"
+            className="rounded bg-blue-500 py-2 px-4 text-sm font-bold text-white hover:bg-blue-700"
+          >
+            追加
+          </button>
         </form>
-        <h3>メンバーリスト</h3>
-        <ul>
-          {members.map((member) => {
+        <h3 className="mb-3 text-lg font-bold">メンバーリスト</h3>
+        {members.length ? (
+          <ol className="list-inside list-decimal">
+            {members.map((member) => {
+              return (
+                <li key={member}>
+                  <div className="inline-flex items-center">
+                    <p className="mr-3 text-sm">{member}</p>
+                    <button
+                      type="button"
+                      className="h-5 w-5 rounded-full bg-red-500 text-sm font-bold leading-[0] text-white hover:bg-red-700"
+                      onClick={() => removeMember(member)}
+                    >
+                      ×
+                    </button>
+                  </div>
+                </li>
+              );
+            })}
+          </ol>
+        ) : (
+          <p className="text-sm text-gray-500">メンバーが追加されていません</p>
+        )}
+      </section>
+
+      <section id={sectionMap.price} className="mb-10">
+        <h2 className="mb-3 text-xl font-bold">使用金額を入力</h2>
+        <p className="mb-3 text-sm">半角数字のみ入力可能です。</p>
+
+        {members.map((member, index) => {
+          return (
+            <div key={member}>
+              <p>{member}</p>
+              <input
+                type="text"
+                name="price"
+                inputMode="decimal"
+                onChange={(event) => handlePrice(event, index)}
+                className="focus:shadow-outline mb-2 w-full appearance-none rounded border py-2 px-3 leading-tight text-gray-700 shadow focus:outline-none"
+              />
+            </div>
+          );
+        })}
+      </section>
+      <section>
+        <h2 className="mb-3 text-xl font-bold">計算結果</h2>
+        <p>
+          合計金額：
+          {totalPrice.toLocaleString("ja-JP", {
+            style: "currency",
+            currency: "JPY",
+          })}
+        </p>
+        <p>
+          一人当たり支出額：
+          {averagePrice.toLocaleString("ja-JP", {
+            style: "currency",
+            currency: "JPY",
+          })}
+        </p>
+        <ol>
+          {members.map((member, index) => {
             return (
-              <li key={member} className="flex">
+              <li key={member}>
                 <p>{member}</p>
-                <button type="button" onClick={() => removeMember(member)}>
-                  削除
-                </button>
+                <p>
+                  {(prices[index] - averagePrice).toLocaleString("ja-JP", {
+                    style: "currency",
+                    currency: "JPY",
+                  })}
+                </p>
               </li>
             );
           })}
-        </ul>
-      </section>
-
-      <section id={sectionMap.price}>
-        <h2>使用金額を入力</h2>
+        </ol>
       </section>
     </DefaultLayout>
   );
